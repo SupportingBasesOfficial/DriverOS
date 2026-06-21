dbf44ce4f24629b894c90c70fd4ae1cc268e72d4
Maure <maure@example.com>
2026-06-21 12:58:38 -0300

    fix build simplify metro config and update easignore

--- metro.config.js
+++ metro.config.js
@@ -1,20 +1,7 @@
 const { getDefaultConfig } = require("expo/metro-config");
-const path = require("path");
 
 const projectRoot = __dirname;
-const workspaceRoot = path.resolve(projectRoot, "../..");
 
 const config = getDefaultConfig(projectRoot);
-
-// Adicionar diretorios ao watchFolders para resolver problema do pnpm
-config.watchFolders = [
-  workspaceRoot,
-  path.resolve(projectRoot, "node_modules/.pnpm"),
-];
-
-config.resolver.nodeModulesPaths = [
-  path.resolve(projectRoot, "node_modules"),
-  path.resolve(workspaceRoot, "node_modules"),
-];
 
 module.exports = config;
