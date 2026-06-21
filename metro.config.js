4eda01afae9cefffbac7b3453091fe22f232ca40
Maure <maure@example.com>
2026-06-21 13:30:31 -0300

    add-supabase-client-and-connection-test

--- metro.config.js
+++ metro.config.js
@@ -4,4 +4,11 @@ const projectRoot = __dirname;
 
 const config = getDefaultConfig(projectRoot);
 
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+  if (moduleName === "@opentelemetry/api") {
+    return { type: "empty" };
+  }
+  return context.resolveRequest(context, moduleName, platform);
+};
+
 module.exports = config;
